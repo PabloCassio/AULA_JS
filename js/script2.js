152e55a98f2a213d7afb4b334fe66a56e1a8da16
Pablo Cassio <82393094+PabloCassio@users.noreply.github.com>
2022-05-18 18:37:48 -0300

    Add files via upload

---
+++ js/script2.js
@@ -0,0 +1,5 @@
+// Escreve na tela, executando script com tag do HTML
+document.write("<h1>Estamos executando o script2.js</h1>");
+
+// Executa script definido por tag script, que aponta para o source script2.js
+document.write("Esse script está em um arquivo externo e é referenciado no final do body do HTML");
